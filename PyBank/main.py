ddf78bc4745eb8161d01cc5e54e6949227afcbf6
carmen-ho <75396980+carmen-ho@users.noreply.github.com>
2021-01-10 22:18:58 -0800

    adding completed hw files

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,45 +6,75 @@ csvpath = os.path.join(".", "Resources", "budget_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # print(csvreader)
+    csv_header = next(csvreader)    # to handle the header
 
-    # csv_header = next(csvreader)
-    # # print(f"CSV Header: {csv_header}")
+    row = 0
+    profit = 0
+    loss = 0
+    net_total = 0
+    row_of_profit = 0
+    row_of_loss = 0
+    # min = 0
+    # max = 0
 
-    # date = list(csvreader)
+    average_pl = 0
+    total_pl = 0
+    changes_pl = []
+    # current_pl = 0
+    # end_pl = 0
+    greatest_increase = ["", 0]
+    greatest_decrease = ["", 9999999999999999999]
 
-    # monthcount = len(date)
-    # print("Total Months: " + str(monthcount))
-
-  
-    # total = 0
-    # for row in csv.reader(csvfile):
-    #     # total = int(row[1])
-    #     total = sum(int(r[1]) for r in csv.reader(csvfile))
-    # print ("Total: $" + str(total))
-
-    # budget_df
-    # budget_df['Change'].mean()
-
-
-
-
-
-
-
-
-
-
-
-
+    month_count = 0
+    month_total = 0
+    
+    month_total += 1
+    first_row = next(csvreader)    # to handle the first month
+    total_pl += int(first_row[1])
+    prev_net = int(first_row[1])   # set the value of first month as previous net
     
 
+    for row in csvreader:
+        
+        month_total += 1
+        total_pl += int(row[1])
+        
+        net_total = net_total + int(row[1])
+        net_change = int(row[1]) - prev_net  # value of current month - value of previous month
+        prev_net = int(row[1])               # update value of previous month
+
+        changes_pl += [net_change]
+
+       
+        if net_change > greatest_increase[1]:
+            greatest_increase[0] = row[0]
+            greatest_increase[1] = net_change
+        
+        if net_change < greatest_decrease[1]:
+            greatest_decrease[0] = row[0]
+            greatest_decrease[1] = net_change
+        
+
+    average_pl = sum(changes_pl) / len(changes_pl)
+
+            
+print("Financial Analysis")
+print("-----------------------")
+print(f"Total Months: {month_total} ")
+print(f"Total: ${total_pl}")
+print(f"Average Change: ${average_pl:.2f}")
+print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
+print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
 
 
+data_output = os.path.join("Analysis", "bankdata.csv")
 
-
-
-
-
-
+with open(data_output, 'w') as csvfile:
+    csvfile.write(f"Financial Analysis\n")
+    csvfile.write(f"------------------------\n")
+    csvfile.write(f"Total Months:{month_total}\n ")
+    csvfile.write(f"Total: ${total_pl}\n")
+    csvfile.write(f"Average Change: ${average_pl:.2f}\n")
+    csvfile.write(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n")
+    csvfile.write(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,6 +6,72 @@ csvpath = os.path.join(".", "Resources", "election_data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # print(csvreader)
-
     csv_header = next(csvreader)
+
+# A complete list of candidates who received votes
+
+    total_votes = 0 
+    khan_votes = 0
+    correy_votes = 0
+    li_votes = 0
+    otooley_votes = 0
+
+    for row in csvreader: 
+
+        total_votes +=1
+
+        if row[2] == "Khan": 
+            khan_votes +=1
+        elif row[2] == "Correy":
+            correy_votes +=1
+        elif row[2] == "Li": 
+            li_votes +=1
+        elif row[2] == "O'Tooley":
+            otooley_votes +=1
+
+candidates = ["Khan", "Correy", "Li","O'Tooley"]
+votes = [khan_votes, correy_votes,li_votes,otooley_votes]
+
+candidates_and_votes = dict(zip(candidates,votes))
+key = max(candidates_and_votes, key=candidates_and_votes.get)
+
+# win = candidates_and_votes.get
+# winner = max(candidates_and_votes, win)
+
+khan_percent = (khan_votes/total_votes) *100
+correy_percent = (correy_votes/total_votes) * 100
+li_percent = (li_votes/total_votes)* 100
+otooley_percent = (otooley_votes/total_votes) * 100
+
+
+print(f"Election Results")
+print(f"------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"------------------------")
+print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Li: {li_percent:.3f}% ({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"-----------------------")
+print(f"Winner: {key}")
+print(f"-----------------------")
+
+
+data_output = os.path.join("Analysis", "bankdata.csv")
+
+with open(data_output,"w") as csvfile:
+
+    csvfile.write(f"Election Results\n")
+    csvfile.write(f"------------------------\n")
+    csvfile.write(f"Total Votes: {total_votes}\n")
+    csvfile.write(f"------------------------\n")
+    csvfile.write(f"Khan: {khan_percent:.3f}% ({khan_votes})\n")
+    csvfile.write(f"Correy: {correy_percent:.3f}% ({correy_votes})\n")
+    csvfile.write(f"Li: {li_percent:.3f}% ({li_votes})\n")
+    csvfile.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})\n")
+    csvfile.write(f"-----------------------\n")
+    csvfile.write(f"Winner: {key}\n")
+    csvfile.write(f"-----------------------")
+
+
+
